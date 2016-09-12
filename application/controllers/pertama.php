95241a1c2b7def532f84c6d8cbc8f5e009cfe471
Dimas Natanael <dimznatanael42@gmail.com>
2016-09-12 12:15:36 +0700

    another commit

---
+++ application/controllers/pertama.php
@@ -0,0 +1,14 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Pertama extends CI_Controller {
+
+	public function index()
+	{
+		echo "index"
+	}
+
+}
+
+/* End of file pertama.php */
+/* Location: ./application/controllers/pertama.php */
